1890be51ecc34b9d815022e65db555ef56e61593
Emmanuel Adiba <eabaagah@gmail.com>
2021-03-05 13:56:40 +0000

    nav layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,20 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
-<body>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Dashboard</a>
+            </li>
+            <li>
+                <a href="">Post</a>
+            </li>
+        </ul>
+    </nav>
     
     @yield('content')
 </body>
