0cbfcd79e4441b69d05ea729d4fe074dbe79ab18
Emmanuel Adiba <eabaagah@gmail.com>
2021-03-17 11:57:12 +0000

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
+use App\Models\Like;
+
 
 class Post extends Model
 {
@@ -16,5 +18,9 @@ class Post extends Model
 
     public function user() {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes() {
+        return $this->hasMany(Like::class);
     }
 }
